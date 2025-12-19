b236a35e4c74e9f19829581186beca629cfc84b9
Elwindria <p.lopez2000@laposte.net>
2025-12-19 17:22:08 +0100

    rajout du controller pour le dashboard + twig

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[IsGranted('ROLE_ADMIN')]
+final class DashboardController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard/index.html.twig', [
+            'controller_name' => 'Admin/DashboardController',
+        ]);
+    }
+}
