25f07030192b723f2da4b58f881ec3dc150ed27c
Boushow <robert.bad.etu@gmail.com>
2025-12-20 18:04:35 -0300

    premier vrai visuel

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,11 @@
 import './styles/app.css';
 import './bootstrap.js';
 
+// Import Bootstrap CSS and JavaScript
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
+// Make bootstrap available globally
+window.bootstrap = bootstrap;
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
