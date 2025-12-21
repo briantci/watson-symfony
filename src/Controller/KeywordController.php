7b51ecfffa1094fb5b6a4913c6269dc68b2aee56
Yamine Daroueche <yaminedaroueche@mailbox.org>
2025-12-21 22:46:23 +0100

    added `/keyword/new` for the keyword creation form

--- src/Controller/KeywordController.php
+++ src/Controller/KeywordController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Keyword;
+use App\Form\KeywordType;
 use App\Repository\KeywordRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,23 +22,23 @@ final class KeywordController extends AbstractController
         ]);
     }
 
-    // #[Route('/new', name: 'app_link_new', methods: ['GET', 'POST'])]
-    // public function new(Request $request, EntityManagerInterface $entityManager): Response
-    // {
-    //     $keyword = new Keyword();
-    //     $form = $this->createForm(KeywordType::class, $keyword);
-    //     $form->handleRequest($request);
+    #[Route('/new', name: 'app_link_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $keyword = new Keyword();
+        $form = $this->createForm(KeywordType::class, $keyword);
+        $form->handleRequest($request);
 
-    //     if ($form->isSubmitted() && $form->isValid()) {
-    //         $entityManager->persist($keyword);
-    //         $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($keyword);
+            $entityManager->flush();
 
-    //         return $this->redirectToRoute('app_admin_dashboard', [], Response::HTTP_SEE_OTHER);
-    //     }
+            return $this->redirectToRoute('app_keyword_index', [], Response::HTTP_SEE_OTHER);
+        }
 
-    //     return $this->render('link/new.html.twig', [
-    //         'link' => $keyword,
-    //         'form' => $form,
-    //     ]);
-    // }
+        return $this->render('link/new.html.twig', [
+            'link' => $keyword,
+            'form' => $form,
+        ]);
+    }
 }
---
+++ src/Form/KeywordType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Keyword;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class KeywordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text', TextType::class, [
+                'label' => 'Nom du mot clé',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Keyword::class,
+        ]);
+    }
+}
